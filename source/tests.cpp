73ebb74a6719da0f5fd9696cb94d814068550dfd
Danielliebmann <liebmann666@gmail.com>
2016-04-21 19:59:49 +0200

    First Commit

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_RUNNER
-#include <catch.hpp>
+#include <catch.hpp>//catchhpp error
 
 int main(int argc, char* argv[])
 {
-  return Catch::Session().run(argc, argv);
+  return Catch::Session().run(argc, argv); 
 }
